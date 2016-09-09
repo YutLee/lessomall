4605295307b410c82805c1edfd31594262e74c38
“me <664009360@qq.com>
2016-09-09 16:22:12 +0800

    ready

--- app/app.js
+++ app/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var test = require('./routes/test');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -31,7 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/test', test);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
